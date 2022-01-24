9534ea75369fb6f9a1da2ee22f95a3a41fffecda
nahanoo <eric.ulrich@unil.ch>
2022-01-24 09:51:30 +0100

    renamed package source

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ setup(name='hgt_detection',
       description="Detects and visualizes horizontal gene transfers in bacterial communities.",
       author='Eric Ulrich',
       url='https://github.com/nahanoo/hgt',
-      packages=['hgt_detection'],
+      packages=['hgt'],
       install_requires=['pandas',
                         'pysam',
                         'Bio',
                         'dna_features_viewer'],
       entry_points={
           'console_scripts': [
-              'detect_hgts = hgt_detection.main:main'
+              'detect_hgts = hgt.main:main'
           ]
       }
       )
